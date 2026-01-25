4f92c425c290406aba16964f9fbdc85ffe6e93db
adrian0t <email@example.com>
2026-01-25 12:29:20 +0000

    Dodanie docker-compose i dwoch kontenerow

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-print("App start...")
-print("zaq")
+import http.server
+import socketserver
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+print(f"Startuje serwer na porcie {PORT}...")
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    httpd.serve_forever()
